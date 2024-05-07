289c011d34c4b274ee51224e40083344bb29880e
Студент <@3-4>
2024-05-07 19:17:48 +0300

    /

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,84 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 15);
-            this.label2.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 39);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Статус:";
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.ForeColor = System.Drawing.SystemColors.Info;
-            this.button1.Location = new System.Drawing.Point(370, 260);
+            this.button1.Location = new System.Drawing.Point(332, 206);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Активация";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(68, 39);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 15);
-            this.label4.TabIndex = 4;
+            this.button1.Size = new System.Drawing.Size(101, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Форма";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
         private Button button1;
-        private Label label4;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -4,89 +4,43 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        Random random = new Random();
+
         public Form1()
         {
-            int x = 100;
-            int y = 100;
             InitializeComponent();
-            for (int i = 0; i < 9; i++)
-            {
-                Button button = new Button()
-                {
-                    Location= new Point(x, y),  
-                    Text = $"{i+1}",
-                    Height= 50,
-                    Width= 100,
-                    BackColor= Color.White
-                };
-                if (i <= 3) Location = new Point(x * i, y);
-                else if (i >= 3 && i <=6) Location = new Point(x * i, y);
-                button.Click += MB;
-                this.Controls.Add(button);
-            }
+            _ = Task.Run(async() => await ButtonLoc());
         }
-        private int POS(int pos, int i)
+
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            if (i == 3 || i == 6 || i == 9)
-            {
-                i = 0;
-                pos *= i;
-            }
-            return pos;
+            this.Hide();
+            Form2 second = new Form2();
+            second.FormClosed += secondFormClose;
+            second.ShowDialog();
         }
-        private void MB (object sender, EventArgs e) { }
-        private void label1_Click(object sender, EventArgs e)
+        private void secondFormClose(object sender, EventArgs e)
         {
-            if (label2.Text == "Vova")
-            {
-                label2.Text = "Matvey";
-            }
-            else
-            {
-                label2.Text = "Vova";
-            }
+            this.Show();
+            
         }
-        private bool sw = false;
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-
-        //    if (sw == false && MessageBox.Show("Ты гей?", "Question", MessageBoxButtons.YesNo) == DialogResult.Yes)
-        //    {
-        //        label4.Text = "GEY";
-        //        sw = true;
-        //    }
-
-        //    else if (sw)
-        //    {
-        //        MessageBox.Show("Не стесняйтя что ты GEY", "Warning!", MessageBoxButtons.OK);
-        //        MessageBox.Show("Молодец, что не стясняешся!", "Warning!", MessageBoxButtons.OK);
-        //        MessageBox.Show("Молодцы сосут концы ;) xD", "Warning!", MessageBoxButtons.OK);
-        //    }
-        //    else if (!sw)
-        //    {
-        //        if (MessageBox.Show("Точно?", "Question", MessageBoxButtons.YesNo) == DialogResult.No)
-        //        {
-        //            label4.Text = "GEY";
-        //        }
-        //        else
-        //        {
-        //            if (MessageBox.Show("Гея ответ!", "Question", MessageBoxButtons.OK) == DialogResult.OK)
-        //            {
-        //                label4.Text = "GEY";
-        //            }
-        //        }
-        //        sw = true;
-        //    }
-        //    //button1.Location = new Point(random.Next(0,800-button1.Size.Height), random.Next(0,500 - button1.Size.Width));
-        //}
-        private void button1_Click(object sender, EventArgs e)
+        public async Task ButtonLoc()
         {
-
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            try
+            {
+                Random random = new Random();
+                while (true)
+                { 
+                     button1.Location = new Point(random.Next(0, 750), random.Next(0, 450));
+                     Thread.Sleep(1000);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+            
         }
     }
 }
