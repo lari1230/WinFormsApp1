8698b1f38fb62072613901728c19907ad37f73a8
Студент <@3-4>
2024-05-28 18:59:11 +0300

    dd

---
+++ WinFormsApp5/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 422);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Редакт";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(786, 416);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(3, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(160, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(631, 370);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 5);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.textBox1, 2);
+            this.textBox1.Size = new System.Drawing.Size(780, 338);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+    }
+}
---
+++ WinFormsApp5/Form1.cs
@@ -0,0 +1,207 @@
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        private static int numPage = 1;
+        TabPage tabPage3 = new TabPage() { Text = "Редакт" + numPage };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        internal void button1_Click(object sender,EventArgs e)
+        {
+            tabControl1.Controls.Add(Create.CreateP(sender, e));
+        }
+
+        internal void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        internal void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.Controls.Remove(tabPage3);
+        }
+    }
+    public static class Create
+    {
+        public static TabPage CreatePage(int indexPage)
+        {
+            var tabPage = new System.Windows.Forms.TabPage();
+            tabPage.SuspendLayout();
+
+            var tableLayoutPanel = CreateLayoutPanel();
+
+            tabPage.Controls.Add(tableLayoutPanel);
+            tabPage.Location = new System.Drawing.Point(4, 24);
+            tabPage.Name = "tabPage2";
+            tabPage.Padding = new System.Windows.Forms.Padding(3);
+            tabPage.Size = new System.Drawing.Size(792, 422);
+            tabPage.TabIndex = 1;
+            tabPage.Text = "Редакт";
+            tabPage.UseVisualStyleBackColor = true;
+
+            return tabPage;
+        }
+        public static TableLayoutPanel CreateLayoutPanel()
+        {
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            tableLayoutPanel.SuspendLayout();
+
+            tableLayoutPanel.ColumnCount = 5;
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            //tableLayoutPanel.Controls.Add(this.button1, 0, 3);
+            //tableLayoutPanel.Controls.Add(this.button2, 1, 3);
+            //tableLayoutPanel.Controls.Add(this.button3, 4, 3);
+            //tableLayoutPanel.Controls.Add(this.textBox1, 0, 0);
+            tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            tableLayoutPanel.Name = "tableLayoutPanel1";
+            tableLayoutPanel.RowCount = 4;
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            tableLayoutPanel.Size = new System.Drawing.Size(786, 416);
+            tableLayoutPanel.TabIndex = 0;
+
+            var textBox = CreateTextBox();
+            tableLayoutPanel.SetColumnSpan(textBox, 5);
+            tableLayoutPanel.SetRowSpan(textBox, 2);
+
+            return tableLayoutPanel;
+        }
+        public static Button CreateButton(string nameButton)
+        {
+            Button button = new Button();
+            button.Dock = System.Windows.Forms.DockStyle.Fill;
+            button.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button.Name = "button1";
+            button.TabIndex = 0;
+            button.Text = nameButton;
+            button.UseVisualStyleBackColor = true;
+
+            return button;
+        }
+        public static TextBox CreateTextBox()
+        {
+            TextBox textBox = new TextBox();
+            textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            textBox.Location = new System.Drawing.Point(3, 3);
+            textBox.Multiline = true;
+            textBox.Name = "textBox1";
+            textBox.Size = new System.Drawing.Size(780, 338);
+            textBox.TabIndex = 3;
+
+            return textBox;
+        }
+        public static TabPage CreateP(object sender, EventArgs e)
+        {
+            var tabPage = new TabPage();
+            var tableLayoutPanel = new TableLayoutPanel();
+            var button1 = new Button();
+            var button2 = new Button();
+            var button3 = new Button();
+            var textBox1 = new TextBox();
+
+            tabPage.Controls.Add(tableLayoutPanel);
+            tabPage.Location = new Point(4, 24);
+            tabPage.Name = "tabPage3";
+            tabPage.Padding = new Padding(3);
+            tabPage.Size = new System.Drawing.Size(792, 422);
+            tabPage.TabIndex = 1;
+            tabPage.Text = "Редакт";
+            tabPage.UseVisualStyleBackColor = true;
+
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel.ColumnCount = 5;
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel.Controls.Add(button1, 0, 3);
+            tableLayoutPanel.Controls.Add(button2, 1, 3);
+            tableLayoutPanel.Controls.Add(button3, 4, 3);
+            tableLayoutPanel.Controls.Add(textBox1, 0, 0);
+            tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            tableLayoutPanel.Name = "tableLayoutPanel1";
+            tableLayoutPanel.RowCount = 4;
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            tableLayoutPanel.Size = new System.Drawing.Size(786, 416);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(3, 370);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(151, 43);
+            button1.TabIndex = 0;
+            button1.Text = "File";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += new System.EventHandler(Form1.button1_Click(sender, e));
+            // 
+            // button2
+            // 
+            button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            button2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(160, 370);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(151, 43);
+            button2.TabIndex = 1;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            button3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(631, 370);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(152, 43);
+            button3.TabIndex = 2;
+            button3.Text = "Close";
+            button3.UseVisualStyleBackColor = true;
+            //button3.Click += Form1.button3_Click();
+            // 
+            // textBox1
+            // 
+            tableLayoutPanel.SetColumnSpan(textBox1, 5);
+            textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            textBox1.Location = new System.Drawing.Point(3, 3);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            tableLayoutPanel.SetRowSpan(textBox1, 2);
+            textBox1.Size = new System.Drawing.Size(780, 338);
+            textBox1.TabIndex = 3;
+
+            tabPage.ResumeLayout(false);
+            tableLayoutPanel.ResumeLayout(false);
+            tableLayoutPanel.PerformLayout();
+
+            return tabPage;
+        }
+    }
+}
